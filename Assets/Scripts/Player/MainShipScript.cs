51870e5bef22aafa08374eafe319b3050ba9d671
bratus <alajoan.jonathan@gmail.com>
2018-07-05 00:47:05 -0300

    COMMIT 1.1
    PUTA QUE ME PARIU 2H CONSERTANDO ESSA MERDA E FUNCIONOU ESTOU SALVANDO NA GLÓRIA DE JAVÉ ESPERO NUNCA MAIS PASSAR POR ISSO DENOVO

---
+++ Assets/Scripts/Player/MainShipScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainShipScript : MonoBehaviour {
+
+    Animator animatorShip;
+    Rigidbody2D playerRigidBody;
+    public float speed;
+    public bool isOnCombat;
+
+	void Start ()
+    {
+        animatorShip = gameObject.GetComponent<Animator>();
+        playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        speed = 10f;
+        isOnCombat = false;
+
+	}
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        float xAxisPhysic;
+        float yAxisPhysic;
+
+        xAxisPhysic = Input.GetAxis("Horizontal");
+        yAxisPhysic = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(xAxisPhysic, yAxisPhysic);
+        playerRigidBody.AddForce(movement * speed);
+
+        
+    }
+
+    void Update()
+    {
+        float xAxis;
+        float yAxis;
+
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+
+        animatorShip.SetFloat("yAxis", yAxis);
+        animatorShip.SetFloat("xAxis", xAxis);
+
+
+    }
+}
