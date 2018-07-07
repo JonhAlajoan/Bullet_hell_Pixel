3613e713ed9d4af12ea7511e5d0570685de22588
bratus <alajoan.jonathan@gmail.com>
2018-07-07 11:00:17 -0300

    Revert "Revert "New functionalities 0.0.2""
    This reverts commit 765fcd11030f7199620f08be62b7cb1d2562c5c6.

--- Assets/Scripts/Player/MainShipScript.cs
+++ Assets/Scripts/Player/MainShipScript.cs
@@ -8,6 +8,9 @@ public class MainShipScript : MonoBehaviour {
     Rigidbody2D playerRigidBody;
     public float speed;
     public bool isOnCombat;
+	public Transform[] Muzzles;
+	protected float nextShotTime;
+	public float msBetweenShots;
 
 	void Start ()
     {
@@ -15,26 +18,11 @@ public class MainShipScript : MonoBehaviour {
         playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
         speed = 10f;
         isOnCombat = false;
-
 	}
 
     // Update is called once per frame
 
     private void FixedUpdate()
-    {
-        float xAxisPhysic;
-        float yAxisPhysic;
-
-        xAxisPhysic = Input.GetAxis("Horizontal");
-        yAxisPhysic = Input.GetAxis("Vertical");
-
-        Vector2 movement = new Vector2(xAxisPhysic, yAxisPhysic);
-        playerRigidBody.AddForce(movement * speed);
-
-        
-    }
-
-    void Update()
     {
         float xAxis;
         float yAxis;
@@ -42,9 +30,80 @@ public class MainShipScript : MonoBehaviour {
         xAxis = Input.GetAxis("Horizontal");
         yAxis = Input.GetAxis("Vertical");
 
-        animatorShip.SetFloat("yAxis", yAxis);
-        animatorShip.SetFloat("xAxis", xAxis);
+		animatorShip.SetFloat("yAxis", yAxis);
+		animatorShip.SetFloat("xAxis", xAxis);
 
+		Vector2 movement = new Vector2(xAxis, yAxis);
+        playerRigidBody.AddForce(movement * speed);
 
+        
     }
+
+	public void ChangeStateOfCombat(bool isCombat)
+	{
+		if(isCombat == true)
+		{
+			isOnCombat = false;
+		}
+
+		if(isCombat == false)
+		{
+			isOnCombat = true;
+		}
+
+
+		animatorShip.SetBool("isOnCombat", isOnCombat);
+	}
+
+	public void Shoot()
+	{
+		if (Time.time > nextShotTime)
+		{
+			for (int i = 0; i < Muzzles.Length; i++)
+			{
+				TrashMan.spawn("BulletMainShip", Muzzles[i].position, Muzzles[i].rotation);
+			}
+			nextShotTime = Time.time + msBetweenShots / 1000;
+		}
+	}
+
+	void Update()
+    {
+
+		/*		for (int i = 0; i < 20; i++)
+				{
+					if (Input.GetKeyDown("joystick 1 button " + i.ToString()))
+					{
+						Debug.Log("joystick 1 button " + i.ToString());
+					}
+				}*/
+
+		if (Input.GetButtonDown("Y button"))
+		{
+			ChangeStateOfCombat(isOnCombat);
+
+		}
+
+		if (isOnCombat == true)
+		{
+			float xAxisRightStick;
+			float yAxisRightStick;
+
+			xAxisRightStick = Input.GetAxis("JRightHorizontal");
+			yAxisRightStick = Input.GetAxis("JRightVertical");
+
+			animatorShip.SetFloat("xAxisRightStick", xAxisRightStick);
+			animatorShip.SetFloat("yAxisRightStick", yAxisRightStick);
+
+			if(Input.GetButton("Fire1"))
+			{
+				Shoot();
+				
+			}
+
+			
+		}
+
+
+	}
 }
